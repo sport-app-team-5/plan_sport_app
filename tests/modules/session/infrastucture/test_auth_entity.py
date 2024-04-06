7c2ed2164b9cec73398c756b2c397423502cc501
miltonb <mj.beltran37@uniandes.edu.co>
2024-04-06 14:31:37 -0500

    fix issue sonar

--- tests/modules/session/infrastucture/test_auth_entity.py
+++ tests/modules/session/infrastucture/test_auth_entity.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from app.modules.auth.domain.entities import Permission, PermissionRole, Role
 from sqlalchemy.orm import Session
 
 def test_permission_role_relationships():
-    permission = Permission(action="read", code="READ", name="Read", created_at=datetime.utcnow())
-    role = Role(code="ADMIN", name="Admin", created_at=datetime.utcnow())
+    
+    permission = Permission(action="read", code="READ", name="Read", created_at=datetime.now(timezone.utc))
+    role = Role(code="ADMIN", name="Admin", created_at=datetime.now(timezone.utc))
     
     permission_role = PermissionRole(permission=permission, role=role)
 
@@ -12,9 +13,9 @@ def test_permission_role_relationships():
     assert permission_role.role == role
 
 def test_permission_str_representation():
-    permission = Permission(action="read", code="READ", name="Read", created_at=datetime.utcnow())
+    permission = Permission(action="read", code="READ", name="Read", created_at=datetime.now(timezone.utc))
     assert str(permission) == "Read"
 
 def test_role_str_representation():
-    role = Role(code="ADMIN", name="Admin", created_at=datetime.utcnow())
+    role = Role(code="ADMIN", name="Admin", created_at=datetime.now(timezone.utc))
     assert str(role) == "Admin"
