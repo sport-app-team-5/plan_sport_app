d99ef5906c9cf4e84dd30c6f36253d8cdf89b210
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-04-06 15:43:15 -0500

    update project

--- app/api/v1/public/sport_men_router.py
+++ app/api/v1/public/sport_men_router.py
@@ -16,18 +16,21 @@ def get_sportsmen(db: Session = Depends(get_db)):
     service = SportsManService()
     return service.get_sportsman(db)
 
+
 @sport_men_router.get("/{sportsman_id}", response_model=SportsManResponseDTO)
 def get_sportsman_by_user_id(sportsman_id: int, db: Session = Depends(get_db)):
     service = SportsManService()
     sportsman = service.get_sportsmen_by_id(sportsman_id, db)
     return sportsman
 
-@sport_men_router.post("", response_model=SportsManResponseDTO, status_code= status.HTTP_201_CREATED)
+
+@sport_men_router.post("", response_model=SportsManResponseDTO, status_code=status.HTTP_201_CREATED)
 def create_sportsman(sportsman_data: SportsManRequestDTO, db: Session = Depends(get_db)):
     service = SportsManService()
     return service.create_sportsmen(sportsman_data, db)
 
-@sport_men_router.put("/{sportsman_id}", response_model=SportsManResponseDTO, status_code= status.HTTP_201_CREATED)
+
+@sport_men_router.put("/{sportsman_id}", response_model=SportsManResponseDTO, status_code=status.HTTP_201_CREATED)
 def update_sportsman(sportsman_id: int, sportsman_data: SportsManRequestDTO, db: Session = Depends(get_db)):
     service = SportsManService()
     return service.update_sportsmen(sportsman_id, sportsman_data, db)
