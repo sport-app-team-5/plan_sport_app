3998e4083980afa21d4748c8c19e40e5673d780b
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-04-13 07:40:45 -0500

    add auth to routes

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -4,7 +4,8 @@ from app.api.v1.private import sport_men_router, allergy_router
 from app.api.v1.private import nutritional_information_router
 
 public_router = APIRouter(prefix="")
-private_router = APIRouter(prefix="")
+
+private_router = APIRouter(prefix="/auth")
 private_router.include_router(session_router)
 private_router.include_router(seeder_router)
 private_router.include_router(allergy_router)
